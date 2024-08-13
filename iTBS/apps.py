d230f54d3adf277ecd1e96f6b3a0a55928107543
Rabotapi01 <202226347@spu.ac.za>
2024-08-13 11:22:40 +0200

    Update apps.py

--- iTBS/apps.py
+++ iTBS/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
 
+from django.db import models
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    student_number = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.student_number})"
+
 
 class ItbsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
