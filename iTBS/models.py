ab705a83610444bb5152aa949894e175e31ea468
Rabotapi01 <202226347@spu.ac.za>
2024-08-13 11:26:00 +0200

    Update models.py

--- iTBS/models.py
+++ iTBS/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Student(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    student_nb = models.CharField(max_length=10, unique=True)
+
+    def __student__(self):
+        return f"{student.first_name} {student.last_name} ({student.student_nb})"
+
 # Create your models here.
